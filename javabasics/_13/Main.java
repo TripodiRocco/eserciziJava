326d25330825937621d7f70331a57e17663d0ab7
Rocco <rocco.tripodi1994@gmail.com>
2023-04-18 19:50:15 +0200

    Exercise 13 added

--- javabasics/_13/Main.java
+++ javabasics/_13/Main.java
@@ -22,7 +22,28 @@ public class Main {
         System.out.println("Exercise 1:");
         ArrayList<Integer> numbers = new ArrayList<>();
 
-        //Write your code here
+        numbers.add(45);
+        numbers.add(421);
+        numbers.add(9);
+        numbers.add(12);
+        numbers.add(1349);
+        numbers.add(1);
+        numbers.add(0);
+        numbers.add(20000);
+
+        System.out.println(numbers.get(0));     // stampa primo elemento
+
+        numbers.remove(0);
+        System.out.println(numbers);    // rimoz primo numero
+
+        numbers.remove(0);
+        System.out.println(numbers);    // rimoz secondo elemento
+
+        numbers.remove(0);        // rimoz terzo elemento
+        System.out.println(numbers);
+
+        System.out.println(numbers.get(0));     // stampa primo elemento rimasto
+
     }
 
     /**
@@ -38,8 +59,26 @@ public class Main {
      * menu item correspond
      */
     public static void exercise2() {
-        System.out.println("\nExercise 1:");
-        //Write your code here
+        System.out.println("\nExercise 2:");
+        ArrayList<String> menuItems = new ArrayList<>();
+        menuItems.add("Insalata di mare");
+        menuItems.add("Carciofi fritti");
+        menuItems.add("Risotto in crema di scampi");
+        menuItems.add("Filetto di manzo ai ferri");
+        menuItems.add("Cheesecake ai frutti di bosco");
+
+        ArrayList<Double> menuPrices = new ArrayList<>();
+        menuPrices.add(11.10);
+        menuPrices.add(9.80);
+        menuPrices.add(12.20);
+        menuPrices.add(15.30);
+        menuPrices.add(6.10);
+
+        System.out.println(menuItems.get(0) + " = " + menuPrices.get(0) + " euro");
+        System.out.println(menuItems.get(1) + " = " + menuPrices.get(1) + " euro");
+        System.out.println(menuItems.get(2) + " = " + menuPrices.get(2) + " euro");
+        System.out.println(menuItems.get(3) + " = " + menuPrices.get(3) + " euro");
+        System.out.println(menuItems.get(4) + " = " + menuPrices.get(4) + " euro");
     }
 
     /**
@@ -57,6 +96,6 @@ public class Main {
         System.out.println("\nExercise 3:");
         int[] items = new int[]{5, 10404, -234348, -53, 9, 91, 92, 34534, 12334, 1435};
 
-        //Write your code here
+        System.out.println(items[0] + items[1] + items[2] + items[3] + items[4] + items [5] + items[6] + items[7] + items[8] + items[9]);
     }
 }
