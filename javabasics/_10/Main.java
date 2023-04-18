65422d1a40f8a1cde2830854691adf6d04da8229
Rocco <rocco.tripodi1994@gmail.com>
2023-04-18 19:40:59 +0200

    Exercise 10 added

--- javabasics/_10/Main.java
+++ javabasics/_10/Main.java
@@ -16,11 +16,11 @@ public class Main {
 
     /**
      * 1: Using an if statement, print: "Comfortable clothes" if the following conditions are met:
-     *
-     *    isWarm is true and wearingShorts is true
-     *    isWarm is false and wearingShorts is false
-     *
-     *    If these conditions are not met, print: "Uncomfortable clothes"
+     * <p>
+     * isWarm is true and wearingShorts is true
+     * isWarm is false and wearingShorts is false
+     * <p>
+     * If these conditions are not met, print: "Uncomfortable clothes"
      */
     private static void exercise1() {
         System.out.println("\nExercise 1 - Comfortable clothes:");
@@ -30,66 +30,105 @@ public class Main {
         boolean isWarm = temperatureInCelsius >= isWarmTemperatureInCelsius;
         boolean wearingShorts = true;
 
-        // Write your code here
+        if (isWarm == wearingShorts || isWarm != wearingShorts) {
+            System.out.println("Comfortable clothes");
+        } else {
+            System.out.println("Uncomfortable clothes");
+        }
     }
 
     /**
      * 2: We are going to automate the club bouncer, write a condition with an if statement with the && (and)
-     *    to check if someone is able to enter the club.
-     *
-     *    Their shoeType must be "fancy" AND they must be over 18 AND they must have at least 10 euros in their pocket
-     *
-     *    Write 1 if statement that checks all of these, print "Come in" if they meet the condition
-     *    or "stay out" if they don't.
+     * to check if someone is able to enter the club.
+     * <p>
+     * Their shoeType must be "fancy" AND they must be over 18 AND they must have at least 10 euros in their pocket
+     * <p>
+     * Write 1 if statement that checks all of these, print "Come in" if they meet the condition
+     * or "stay out" if they don't.
      */
     private static void exercise2() {
         System.out.println("\nExercise 2 - Club Bouncer:");
         String shoeType = "fancy";
 
 
-
         int age = 30;
         double eurosInPocket = 7.5;
         double eurosForEntry = 10;
 
-        // Write your code here
+        boolean shoes = shoeType == "fancy";
+        boolean ageForEntry = age > 18;
+        boolean remainingEuros = eurosForEntry == 10;
+
+
+        if (shoes && ageForEntry && remainingEuros) {
+            System.out.println("Come in");
+        } else {
+            System.out.println("Stay out");
+        }
+
     }
+
 
     /**
      * 3: Write an if else statement to calculate our grade
-     *
-     *    If our grade is 90%+ of the total, we print "A"
-     *    If our grade is 80%+ of the total, we print "B"
-     *    If our grade is 70%+ of the total, we print "C"
-     *    If our grade is 60%+ of the total, we print "D"
-     *    If our grade is under 60% of the total, we print "F"
-     *
-     *    If 'isTestRetry' we cap the maximum grade at C
-     *
-     *    Remember was can use ! to invert the value of a boolean, i.e. true -> false
+     * <p>
+     * If our grade is 90%+ of the total, we print "A"
+     * If our grade is 80%+ of the total, we print "B"
+     * If our grade is 70%+ of the total, we print "C"
+     * If our grade is 60%+ of the total, we print "D"
+     * If our grade is under 60% of the total, we print "F"
+     * <p>
+     * If 'isTestRetry' we cap the maximum grade at C
+     * <p>
+     * Remember was can use ! to invert the value of a boolean, i.e. true -> false
      */
     private static void exercise3() {
         System.out.println("\nExercise 3 - Test Grader:");
         double ourTestScore = 8;
         double maxTestScore = 10;
         boolean isTestRetry = true;
-        //Write your code here
+
+        double ourGrade = ourTestScore / maxTestScore * 100;
+
+        if (ourGrade >= 90) {
+            System.out.println("A");
+            isTestRetry = false;
+        } else if (ourGrade >= 80 && ourGrade < 90) {
+            System.out.println("B");
+            isTestRetry = false;
+        } else if (ourGrade >= 70 && ourGrade < 80) {
+            System.out.println("C");
+            isTestRetry = false;
+        } else if (ourGrade >= 60 && ourGrade < 70) {
+            System.out.println("D");
+            isTestRetry = false;
+        } else if (ourGrade >= 50 && ourGrade < 70) {
+            System.out.println("F");
+        } else if (isTestRetry){
+            System.out.println("Ci dispiace, ma non hai raggiunto il punteggio necessario per superare il test. Riprova!");
+        }
+
+
     }
 
 
     /**
      * 4: BONUS! This is an optional task, but it is recommended you complete it
      * ---------------------------------------------------------------------------------
-     *    Write a XOR condition, print "safe" if only one car is driving down the road,
-     *    print "CRASH! or No cars" if not.
-     *
-     *    XOR = ^
+     * Write a XOR condition, print "safe" if only one car is driving down the road,
+     * print "CRASH! or No cars" if not.
+     * <p>
+     * XOR = ^
      */
     private static void exercise4Bonus() {
         System.out.println("\nExercise 4 (Optional) - One way road:");
         boolean carIsDrivingFromLeftToRight = true;
         boolean carIsDrivingFromRightToLeft = false;
 
-        //Write your code here
+        if (carIsDrivingFromLeftToRight ^ carIsDrivingFromRightToLeft) {
+            System.out.println("safe");
+        } else {
+            System.out.println("CRASH! or No cars");
+        }
     }
 }
