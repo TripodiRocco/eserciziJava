9b9c4ec731cef20e91f5443900c089ded6943e36
Rocco <rocco.tripodi1994@gmail.com>
2023-04-23 00:25:54 +0200

    Exercise 2 in OOP: class Student added

--- javaoop/exercises/_2/Student.java
+++ javaoop/exercises/_2/Student.java
@@ -1,4 +1,35 @@
 package javaoop.exercises._2;
 
+
+
 public class Student {
+    String name = "Rocco";
+    int age = 29;
+
+
+
+
+    Student(String name, int age){
+        this.name = name;
+        this.age = age;
+        System.out.println("Constructing!");
+        System.out.println("Constructed!");
+    }
+
+
+    // esercizio 2
+    Student(int age){      // primo costruttore
+        this.age = age;
+
+    }
+
+    Student(String name){      // secondo costruttore
+        this.name = name;
+
+    }
+
+    Student(){      // terzo costruttore
+
+    }
+
 }
